476a541be12eb7a2f46e77528ec6462b51f93ccf
haribabu <31000077@carandbike.com>
2023-12-20 14:05:03 +0530

    removing un used code

--- components/home.js
+++ components/home.js
@@ -1,144 +1,125 @@
-import { Text,View,StyleSheet,Image } from "react-native"
-import Input from "./Input"
-import { useEffect, useState } from "react"
-import Btn from "./btn"
-import {useNavigation} from "@react-navigation/native"
+import { Text, View, StyleSheet, Image } from "react-native";
+import Input from "./Input";
+import { useEffect, useState } from "react";
+import Btn from "./btn";
+import { useNavigation } from "@react-navigation/native";
 import { Alert } from "react-native";
-import {LOGDATA} from "../constants/data"
+import { LOGDATA } from "../constants/data";
 
+export default function Home(props) {
+  const { isLogin } = props;
+  const navigation = useNavigation();
+  const [number, setNumber] = useState();
+  const [pwd, setPwd] = useState();
+  const [name, setName] = useState();
+  const [isHide, setHide] = useState(true);
+  const [isSnackbar, setIsSnackbar] = useState(false);
+  const [snackbarMsg, setSnackbarMsg] = useState("");
 
-export default function  Home(props){
-    const{isLogin}=props
-    const navigation=useNavigation()
-    const [number,setNumber]=useState();
-    const [pwd,setPwd]=useState();
-    const [name,setName]=useState();
-    const [isHide,setHide]=useState(true);
-    const[isSnackbar,setIsSnackbar]=useState(false)
-    const [snackbarMsg,setSnackbarMsg]=useState("")
-
-
-   
-
-   
-
-    const changeHandler=(event,placeholder)=>{
-     switch(placeholder){
-        case "Enter Mobile Number":
-                    setNumber(event)
-                    break;
-        case "Enter password":
-                    setPwd(event)
-                    break;
-        case "Enter Name":
-                     setName(event);
-                     break;
-       }
-
-        
-
+  const changeHandler = (event, placeholder) => {
+    switch (placeholder) {
+      case "Enter Mobile Number":
+        setNumber(event);
+        break;
+      case "Enter password":
+        setPwd(event);
+        break;
+      case "Enter Name":
+        setName(event);
+        break;
     }
-    const btnPressHandler=()=>{
-        if(isLogin){
-      
-        const validataion =LOGDATA.find((data)=>data.mobNumber==number && data.password==pwd)
-        
-        if(validataion){
-            // setSnackbarMsg("Login Successfully")
-            // setIsSnackbar(true)
-            navigation.navigate("Recharge")
-           
-        }
-        else{
-            Alert.alert("Please Enter correct details");
-            // setSnackbarMsg("Please Enter correct details")
-            // setIsSnackbar(true)
-        }
-    }
-    else{
-        if(name&&number&&pwd){
-           const data=
-            {
-                name:name,
-                mobNumber:number,
-                password:pwd
-            }
-           
-           LOGDATA.push(data)
-        //    setSnackbarMsg("Your account is created Please do login")
-        //    setIsSnackbar(true)
-           navigation.navigate("Login")
-        }
-        else{
-            Alert.alert("Please Enter correct details")
-            // setSnackbarMsg("Please Enter correct details")
-            // setIsSnackbar(true)
-        }
-    }
+  };
+  const btnPressHandler = () => {
+    if (isLogin) {
+      const validataion = LOGDATA.find(
+        (data) => data.mobNumber == number && data.password == pwd
+      );
 
+      if (validataion) {
+        navigation.navigate("Recharge");
+      } else {
+        Alert.alert("Please Enter correct details");
+      }
+    } else {
+      if (name && number && pwd) {
+        const data = {
+          name: name,
+          mobNumber: number,
+          password: pwd,
+        };
 
+        LOGDATA.push(data);
+        navigation.navigate("Login");
+      } else {
+        Alert.alert("Please Enter correct details");
+      }
     }
-    const linkHandler=()=>{
-       isLogin? navigation.navigate("Signup"): navigation.navigate("Login")
+  };
+  const linkHandler = () => {
+    isLogin ? navigation.navigate("Signup") : navigation.navigate("Login");
+  };
 
-    }
+  const iconHandler = () => {
+    setHide(!isHide);
+  };
 
-    const iconHandler=()=>{
-        setHide(!isHide)
-    }
-
-    return(
-       <View  style={styles.container}> 
-         <View style={styles.imgContainer}>
-           <Image
-             source={require('../assets/logo.jpg')}
-             style={styles.imageStyle}
-           
-           />
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.imgContainer}>
+          <Image
+            source={require("../assets/logo.jpg")}
+            style={styles.imageStyle}
+          />
         </View>
-     
-           {(!isLogin) &&
-             <Input
-             type="text"
-             value={name}
-             changeHandler={changeHandler}
-             placeholder="Enter Name"
 
-         />
-           }
-          
+        {!isLogin && (
           <Input
-             type="number"
-             value={number}
-             changeHandler={changeHandler}
-             placeholder="Enter Mobile Number"
-             keyboardType="numeric"
-         />
+            type="text"
+            value={name}
+            changeHandler={changeHandler}
+            placeholder="Enter Name"
+          />
+        )}
 
         <Input
-             type="password"
-             value={pwd}
-             changeHandler={changeHandler}
-             placeholder="Enter password"
-             keyboardType="numeric"
-             isSecure={isHide}
-             iconHandler={iconHandler}
-         />
+          type="number"
+          value={number}
+          changeHandler={changeHandler}
+          placeholder="Enter Mobile Number"
+          keyboardType="numeric"
+        />
 
-         <Btn
-           title={isLogin? "Login":"Sign up"}
-           onPress={btnPressHandler}
-         
-         />
+        <Input
+          type="password"
+          value={pwd}
+          changeHandler={changeHandler}
+          placeholder="Enter password"
+          keyboardType="numeric"
+          isSecure={isHide}
+          iconHandler={iconHandler}
+        />
 
-         <Text style={styles.textStyle}>
-            {
-                isLogin?
-               <Text>  If You Don't have account try to <Text style={styles.link} onPress={linkHandler}>Signup</Text></Text>
-                :
-               <Text> If You  have account try to <Text style={styles.link} onPress={linkHandler}>Login</Text> </Text>
-            }
-           
+        <Btn title={isLogin ? "Login" : "Sign up"} onPress={btnPressHandler} />
+
+        <Text style={styles.textStyle}>
+          {isLogin ? (
+            <Text>
+              {" "}
+              If You Don't have account try to{" "}
+              <Text style={styles.link} onPress={linkHandler}>
+                Signup
+              </Text>
+            </Text>
+          ) : (
+            <Text>
+              {" "}
+              If You have account try to{" "}
+              <Text style={styles.link} onPress={linkHandler}>
+                Login
+              </Text>{" "}
+            </Text>
+          )}
         </Text>
         {/* {
             isSnackbar &&
@@ -151,37 +132,56 @@ export default function  Home(props){
              
              />
         } */}
-         
-
-       </View>
-    )
-
+      </View>
+      {/* <View style={{position:'relative'}} >
+          <View style={styles.customerContainer}>
+               <Text style={styles.customerText}>Contact us : developer.haribabuperla@gmail.com</Text>
+        </View>
+       </View> */}
+    </>
+  );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        marginVertical:20,
-        padding:20
-    },
-    imgContainer:{
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    textStyle:{
-        fontSize:16,
-        marginTop:10
-    },
-    link:{
-        color:"blue",
-        textDecorationLine:"underline"
-    },
-    imageStyle:{
-        height:200,
-        width:200,
-        borderRadius:100,
-        marginBottom:20,
-        borderWidth:2,
-        borderColor:'black'
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+    padding: 20,
+  },
+  imgContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textStyle: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  link: {
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+  imageStyle: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  customerContainer: {
+    position: "absolute",
+    top: "-900%",
+    width: "100%",
+  },
 
-    }
-})
+  customerText: {
+    backgroundColor: "#3a9cde",
+    color: "white",
+    width: "100%",
+    paddingVertical: 20,
+    textAlign: "center",
+    fontSize: 18,
+    borderWidth: 2,
+    borderColor: "orange",
+    borderStyle: "dotted",
+  },
+});
